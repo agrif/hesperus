6e6b7cffa43a4b65f29475ccb059d6be451311e4
Aaron Griffith <aargri@gmail.com>
2011-05-03 23:32:37 -0400

    new basic (undertested) twitter announce plugin

---
+++ hesperus/plugins/twit.py
@@ -0,0 +1,46 @@
+from ..plugin import PollPlugin
+from ..core import ET, ConfigurationError
+from ..shorturl import short_url
+import time
+import twitter
+
+class TwitterPlugin(PollPlugin):
+    poll_interval = 50
+    
+    @PollPlugin.config_types(usernames=ET.Element)
+    def __init__(self, core, usernames=None):
+        super(TwitterPlugin, self).__init__(core)
+        
+        self.usernames = []
+        self.last_update = time.time()
+
+        if usernames == None:
+            usernames = []
+        for el in usernames:
+            if not el.tag.lower() == 'username':
+                raise ConfigurationError('usernames must contain username tags')
+            self.usernames.append(el.text)
+        
+        self.twitter = twitter.Api()
+        
+    def poll(self):
+        new_tweets = []
+        for username in self.usernames:
+            statuses = self.twitter.GetUserTimeline(username)
+            yield
+            statuses = filter(lambda s: s.created_at_in_seconds > self.last_update, statuses)
+            statuses = filter(lambda s: not s.text.startswith("@"), statuses)
+            yield
+            new_tweets += statuses
+        yield
+        
+        new_tweets = sorted(new_tweets, key=lambda s: s.created_at_in_seconds)
+        yield
+        
+        for tweet in new_tweets:
+            msg = "@{s.user.screen_name} tweeted: {s.text}"
+            msg = msg.format(s=tweet)
+            for chan in self.channels:
+                self.parent.send_outgoing(chan, msg)
+            self.log_message(msg)
+            yield

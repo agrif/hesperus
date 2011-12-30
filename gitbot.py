a2ce631f2765fd9bbc558af0f2cce619a32e0258
Aaron Griffith <aargri@gmail.com>
2011-12-30 14:20:10 -0500

    removed extra imports from gitbot.py

--- gitbot.py
+++ gitbot.py
@@ -1,9 +1,5 @@
 from hesperus.core import Core
-from hesperus.plugin import Plugin
-from hesperus.plugins.irc import IRCPlugin
-import time, random, sys
-
-###########################
+import sys
 
 c = Core.load_from_file(sys.argv[1])
 try:

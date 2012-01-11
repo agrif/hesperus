f10004bef48b33c605560bd421a1bcae4d146e2d
Andrew Brown <brownan@gmail.com>
2012-01-11 11:38:33 -0500

    Added regular expression matching to EchoPlugin

--- hesperus/plugins/echo.py
+++ hesperus/plugins/echo.py
@@ -1,30 +1,54 @@
+import re
+
 from hesperus.plugin import CommandPlugin
 from ..core import ConfigurationError, ET
 from ..shorturl import short_url
 
 class EchoPlugin(CommandPlugin):
+    """When someone says a line of text directly to the bot, the bot will
+    respond as configured.
+
+    Does simple text or regular expression matching on the input and responds
+    with a static line for output.
+
+    Takse one configuration element: <commands>
+    Each <command> element can have either a names attribute: a comma separated
+    list of text to match on, or an re attribute: a regular expression to
+    match. The text of the <command> element will be the response.
+
+    """
     @CommandPlugin.config_types(commands = ET.Element)
     def __init__(self, core, commands=None):
         super(CommandPlugin, self).__init__(core)
         
         self.commands = {}
+        self.matchers = {}
         
         if commands == None:
             commands = []
         for el in commands:
             if not el.tag.lower() == 'command':
                 raise ConfigurationError('commands must contain command tags')
-            names = el.get('names', None)
-            if names == None:
-                raise ConfigurationError('command tags must have a names attribute')
+            names = el.get('names', "")
+            expression = el.get("re", None)
+            if not names and not expression:
+                raise ConfigurationError('command tags must have either a names or an re attribute')
+
             text = el.text.strip()
             
             for name in names.split(","):
                 self.commands[name.lower()] = text
+
+            if expression:
+                self.matchers[ re.compile(expression, re.I) ] = text
         
     @CommandPlugin.register_command(r"(\S+)")
     def echo_command(self, chans, name, match, direct, reply):
         cmd = match.group(1).lower()
-        if not cmd in self.commands:
-            return
-        reply("%s" % (self.commands[cmd],))
+        if cmd in self.commands:
+            reply("%s" % (self.commands[cmd],))
+        else:
+            for reobj, text in self.matchers.iteritems():
+                if reobj.match(cmd):
+                    reply(text)
+                    break

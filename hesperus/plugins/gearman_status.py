3745439bce688a7103af638629172bd70dee58f4
aheadley <aheadley@waysaboutstuff.com>
2012-03-27 16:02:42 -0400

    add filter support for workers and jobs commands

--- hesperus/plugins/gearman_status.py
+++ hesperus/plugins/gearman_status.py
@@ -1,5 +1,6 @@
 import gearman
 from hesperus.plugin import CommandPlugin
+import re
 
 class GearmanStatusPlugin(CommandPlugin):
     @CommandPlugin.config_types(gearman_host=str)
@@ -7,24 +8,33 @@ class GearmanStatusPlugin(CommandPlugin):
         super(GearmanStatusPlugin, self).__init__(core)
         self.admin_client = gearman.GearmanAdminClient([gearman_host])
 
-    @CommandPlugin.register_command(r"jobs")
+    @CommandPlugin.register_command(r"jobs(?:\s+(.*))?")
     def job_status_command(self, chans, name, match, direct, reply):
         running = []
         idle = []
+        if match.group(1):
+            fltr = lambda t: re.search(match.group(1), t['task'])
+        else:
+            fltr = lambda t: True
         for task in self.admin_client.get_status():
-            if task['running'] == 0 and task['queued'] == 0:
-                idle.append(task['task'])
-            else:
-                running.append('%s: %d+%d/%d' % (task['task'], task['running'], task['queued'], task['workers']))
+            if fltr(task):
+                if task['running'] == 0 and task['queued'] == 0:
+                    idle.append(task['task'])
+                else:
+                    running.append('%s: %d+%d/%d' % (task['task'], task['running'], task['queued'], task['workers']))
         if idle:
             reply('Idle tasks: ' + ', '.join(idle))
         for task in running:
             reply(task)
 
-    @CommandPlugin.register_command(r"workers")
+    @CommandPlugin.register_command(r"workers(?:\s+(.*))?")
     def worker_status_command(self, chans, name, match, direct, reply):
         workers = []
+        if match.group(1):
+            fltr = lambda w: any(map(lambda t: re.search(match.group(1), t), w['tasks']))
+        else:
+            fltr = lambda w: True
         for worker in self.admin_client.get_workers():
             #admin client's client_id is '-'
-            if worker['client_id'] != '-':
+            if worker['client_id'] != '-' and fltr(worker):
                 reply('%s[%s]: %s' % (worker['client_id'], worker['ip'], ', '.join(worker['tasks'])))

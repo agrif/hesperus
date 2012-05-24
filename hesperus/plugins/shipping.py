298380e81bf1c0e0b6e89dd28e85dec0bd1a7628
aheadley <aheadley@waysaboutstuff.com>
2012-05-23 22:53:17 -0400

    handle errors better

--- hesperus/plugins/shipping.py
+++ hesperus/plugins/shipping.py
@@ -2,6 +2,7 @@ import packagetrack
 import time
 
 from ..plugin import PollPlugin, CommandPlugin
+from ..shorturl import short_url
 
 class FollowingPlugin(PollPlugin, CommandPlugin):
     pass
@@ -10,16 +11,26 @@ class TrackingPlugin(CommandPlugin):
     @CommandPlugin.register_command(r'track\s+([\w\d]+)')
     def track_command(self, chans, name, match, direct, reply):
         tn = match.group(1)
-        package = packagetrack.Package(match.group(1))
+        package = packagetrack.Package(tn)
         try:
             info = package.track()
+        except packagetrack.UnsupportedShipper:
+            self.log_warning('UnsupportedShipper: {}'.format(tn))
+            reply('Dunno any shippers for a number like that')
+        except packagetrack.service.InvalidTrackingNumber:
+            self.log_warning('InvalidTrackingNumber: {}'.format(tn))
+            reply('Are you sure you that\'s the right number?')
         except Exception as e:
-            reply('OH NO: {error}'.format(error=e.message))
+            msg = '({tn}) {etype}: {message}'.format(
+                etype=e.__class__.__name__, message=e.message, tn=tn)
+            self.log_warning(msg)
+            reply(msg)
         else:
-            msg = '{carrier} has your stuff at {status} as of {last_update}, '+ \
-                'you can expect it dumped on your doorstep on {delivery_date}'
+            msg = '{carrier} has it at {status} as of {last_update}, '+ \
+                'should be delivered on {delivery_date} ({url})'
             reply(msg.format(
                 carrier=package.shipper,
                 status=info.status,
                 last_update=info.last_update.strftime('%m/%d %H:%M'),
-                delivery_date=info.delivery_date.strftime('%m/%d'))
+                delivery_date=info.delivery_date.strftime('%m/%d'),
+                url=short_url(package.url())))

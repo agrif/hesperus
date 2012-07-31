b9572a7f4766d36b2d3b51e21390dc215c8e5aea
aheadley <aheadley@waysaboutstuff.com>
2012-07-31 17:53:20 -0400

    why did i do this

--- hesperus/plugins/annoying.py
+++ hesperus/plugins/annoying.py
@@ -2,7 +2,7 @@ import random
 import time
 import re
 
-from ..plugin import Plugin
+from ..plugin import Plugin, PassivePlugin
 from ..core  import ET
 
 class Repeater(Plugin):
@@ -124,3 +124,33 @@ class ORLY(Plugin):
             self.lastmsg = time.time()
             reply("YA RLY!")
             return
+
+PRONOUN_MAP = {
+    'he': ['she', 'her'],
+    'she': ['he', 'his'],
+    'it': ['I', 'my', 'self'],
+}
+
+class ThatsWhatSheSaid(PassivePlugin):
+    PHRASE = 'That\'s what {pronoun} said!'
+    
+    @PassivePlugin.config_types(chance=float)
+    def __init__(self, core, chance=0.9, *args):
+        super(ThatsWhatSheSaid, self).__init__(core, *args)
+        self._chance = chance
+    
+    @PassivePlugin.register_pattern(r'\b(%s)\b' % '|'.join(k for p in PRONOUN_MAP for k in PRONOUN_MAP[p]))
+    def misogyny(self, match, reply):
+        pn = match.group(1)
+        self.log_debug('Hit on pronoun: %s' % pn)
+        roll = random.random()
+        if roll > self._chance:
+            self.log_debug('Replying, roll was %f' % roll)
+            reply(self.PHRASE.format(pronoun=self._get_pronoun(pn)))
+    
+    def _get_pronoun(self, pn):
+        for k in PRONOUN_MAP:
+            if pn in PRONOUN_MAP[k]:
+                return k
+        else:
+            return 'she'    
